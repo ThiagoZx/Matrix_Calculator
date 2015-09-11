6094d08430935cb5cbf89f9890f785868a241e63
Thiago Torres <TheThiagoo0@gmail.com>
2015-09-11 10:17:49 -0300

    Matrix On!
    Due code changes, the button now does generate a grid with the
    considered numbers.

--- Matrix_Calculator/Matrix_Calculator/MainWindow.xaml.cs
+++ Matrix_Calculator/Matrix_Calculator/MainWindow.xaml.cs
@@ -28,8 +28,25 @@ namespace Matrix_Calculator
         void returnTextValue() {
 
         }
-        void generateGrid() { 
-        
+        void generateGrid(object sender, RoutedEventArgs e) {
+            TextBox line = (TextBox)this.FindName("Line_Input");
+            TextBox column = (TextBox)this.FindName("Column_Input");
+            int lines_int = Convert.ToInt32(line.Text);
+            int column_int = Convert.ToInt32(column.Text);
+
+            for (int i = 1; i <= lines_int; i++) {
+                for (int j = 1; j <= column_int; j++) {
+
+                    TextBox tb = new TextBox();
+                    tb.Name = ("Matrix_" + i + "_" + j).ToString();
+                    tb.Margin = new Thickness(0, 0, 50 + i * 45, 50 + j * 45);
+                    tb.Width = 20; tb.Height = 20;
+                    Console.WriteLine(tb.TextAlignment.ToString());
+
+                    Tab_01.Children.Add(tb);
+
+                }
+            }
         }
     }
 }
