89639229f18584fbdf6271c2c3e7c6913553d7d4
Thiago Torres <TheThiagoo0@gmail.com>
2015-09-22 09:21:16 -0300

    Transponha-se!
    Done.

--- Matrix_Calculator/PropertiesManager.cs
+++ Matrix_Calculator/PropertiesManager.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 public class PropertiesManager : Window {
-    
+
+    public string[] transposed () {
+        TextBox line = (TextBox)this.FindName ("Line_Input");
+        TextBox column = (TextBox)this.FindName ("Column_Input");
+        int lines = Convert.ToInt32 (line.Text);
+        int columns = Convert.ToInt32 (column.Text);
+
+        string[] results = new string[lines * columns];
+
+        for (int i = 1; i <= columns; i++) {
+            for (int j = 1; j <= lines; j++) {
+                double element_a = getValue ("Matrix_" + j + "_" + i + "_1");
+                results[results.Length] = result.ToString ();
+            }
+        }
+
+        return results;
+    }
+
+
 }
