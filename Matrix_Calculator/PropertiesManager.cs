5954979a9a359c8b23463d638a6b3465d33c0fe8
Thiago Torres <TheThiagoo0@gmail.com>
2015-09-24 09:00:16 -0300

    Opostas!
    A propriedade mais fácil de toda essa calculadora. Menos de 3 minutos?
    Muito bom. Correção de sintaxe em outras funções

--- Matrix_Calculator/PropertiesManager.cs
+++ Matrix_Calculator/PropertiesManager.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 
 public class PropertiesManager : Window {
 
+    private double getValue (string name) {
+
+        TextBox tbx = (TextBox)this.FindName (name);
+        string text = tbx.Text;
+        double value = Convert.ToDouble (text);
+
+        return value;
+
+    }
     public string[] transposed () {
         TextBox line = (TextBox)this.FindName ("Line_Input");
         TextBox column = (TextBox)this.FindName ("Column_Input");
@@ -14,7 +23,26 @@ public class PropertiesManager : Window {
         for (int i = 1; i <= columns; i++) {
             for (int j = 1; j <= lines; j++) {
                 double element_a = getValue ("Matrix_" + j + "_" + i + "_1");
-                results[results.Length] = result.ToString ();
+                results[results.Length] = element_a.ToString ();
+            }
+        }
+
+        return results;
+    }
+
+    public string[] oposite () {
+        TextBox line = (TextBox)this.FindName ("Line_Input");
+        TextBox column = (TextBox)this.FindName ("Column_Input");
+        int lines = Convert.ToInt32 (line.Text);
+        int columns = Convert.ToInt32 (column.Text);
+
+        string[] results = new string[lines * columns];
+
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= columns; j++) {
+                double element_a = getValue ("Matrix_" + j + "_" + i + "_1");
+                element_a = element_a * (-1);
+                results[results.Length] = element_a.ToString ();
             }
         }
 
