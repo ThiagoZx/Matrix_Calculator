7343c1b9056f7c456857697fe216d126f2b9a430
ThiagoZx <TheThiagoo0@gmail.com>
2015-09-27 02:23:16 -0300

    !asrevnI
    Falta pouco agora.

--- Matrix_Calculator/PropertiesManager.cs
+++ Matrix_Calculator/PropertiesManager.cs
@@ -10,6 +10,7 @@ public class PropertiesManager : Window {
         double value = Convert.ToDouble (text);
 
         return value;
+
     }
 
     public string[] transposed () {
@@ -28,6 +29,7 @@ public class PropertiesManager : Window {
         }
 
         return results;
+
     }
 
     public string[] oposite () {
@@ -47,6 +49,7 @@ public class PropertiesManager : Window {
         }
 
         return results;
+
     }
 
     public bool isSimetric(string[] matrix) {
@@ -62,6 +65,7 @@ public class PropertiesManager : Window {
 
         MessageBoxResult result = MessageBox.Show("Essa matriz é simétrica!", "Calculadora diz:");
         return true;
+
     }
 
     private double splitElement(string[] matrix, int line, int column){
@@ -71,6 +75,7 @@ public class PropertiesManager : Window {
                 return Convert.ToDouble(splited[0]); 
             }
         }
+
     }
     
     private string[] splitMatrix(string[] matrix, int line, int column){
@@ -90,17 +95,31 @@ public class PropertiesManager : Window {
         }
 
         return rstMatrix;
+
     }
 
     private string cofactor(string[] matrix, int line, int column){
 
         string[] elements = splitMatrix(matrix, line, column);
 
-        if(elements.Length == 9){
-            return Math.Pow((-1), line + column) * Convert.ToDouble(determinant_n3(elements));
-        } else{
-            return Math.Pow((-1), line + column) * Convert.ToDouble(determinant_nn(elements));
-        }
+        switch (elements.Length) {
+            case 1:
+                return (Math.Pow((-1), line + column) * getValue("Matrix_1_1_1")).ToString();
+                break;
+
+            case 4:
+                return (Math.Pow((-1), line + column) * Convert.ToDouble(determinant_n2(elements))).ToString();
+                break;
+
+            case 9:
+                return (Math.Pow((-1), line + column) * Convert.ToDouble(determinant_n3(elements))).ToString();
+                break;
+
+            default:
+                return (Math.Pow((-1), line + column) * Convert.ToDouble(determinant_nn(elements))).ToString();
+                break;
+        }   
+
     }
 
     private string determinant_n2(string[] matrix) {
@@ -110,6 +129,7 @@ public class PropertiesManager : Window {
         double dtrmnt = princ - secnd;
 
         return dtrmnt.ToString();
+
     }
 
     private string determinant_n3(string[] matrix) {
@@ -154,11 +174,12 @@ public class PropertiesManager : Window {
         double result = 0;
 
         for(int i = 1; i <= Math.Sqrt(matrix.Length); i++){
-            double element = splitElement(matrix, 1, i) * cofactor(matrix, 1, i);
+            double element = splitElement(matrix, 1, i) * Convert.ToDouble(cofactor(matrix, 1, i));
             result = result + element;
         }
 
         return result.ToString();
+
     }
 
     public string determinant() {
@@ -198,6 +219,78 @@ public class PropertiesManager : Window {
             default:
                 return determinant_nn(elements);
                 break;
-        }
+        }       
+    
     }
+
+    public string[] inverse(){
+        TextBox line = (TextBox)this.FindName("Line_Input");
+        TextBox column = (TextBox)this.FindName("Column_Input");
+        int lines = Convert.ToInt32(line.Text);
+        int columns = Convert.ToInt32(column.Text);
+
+        string[] elements = new string[lines * columns];
+
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= columns; j++) {
+                string element_a = getValue("Matrix_" + i + "_" + j + "_1").ToString();
+                element_a = element_a + "$" + i + "_" + j;
+                elements[elements.Length] = element_a.ToString();
+            }
+        }
+
+        double dtrmnt = Convert.ToDouble(determinant_nn(elements));
+
+        if (dtrmnt == 0 || lines != columns) {
+            MessageBoxResult result = MessageBox.Show("Essa matriz não possui inversa!", "Calculadora diz:");
+            string[] badresult = new string[1];
+            badresult[badresult.Length] = "Bad Result!";
+            return badresult;
+        }
+
+        string cofactors = new string[lines * columns]
+            
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= columns; j++) {
+                double cofactor = Convert.ToDouble(cofactor(elements, i, j));
+                cofactor = cofactor * 1 / dtrmnt;
+                cofactors[cofactors.Length] = cofactor.ToString();
+            }
+        }
+        
+        return cofactors;
+
+    }
+
+    public bool isInverse(){
+        TextBox line = (TextBox)this.FindName ("Line_Input_2");
+        TextBox column = (TextBox)this.FindName ("Column_Input_2");
+        int lines = Convert.ToInt32 (line.Text);
+        int columns = Convert.ToInt32 (column.Text);
+
+        string[] invrs = new string[lines * columns];
+        string[] matrix = this.inverse();
+
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= columns; j++) {
+                double element_a = getValue ("Matrix_" + i + "_" + j + "_2");
+                element_a = element_a * (-1);
+                results[results.Length] = element_a.ToString ();
+            }
+        }
+
+        for (int i = 0; i < matrix.Length; i++) {
+            if (invrs[i] != matrix[i]) {
+                MessageBoxResult result = MessageBox.Show("Essa matriz não é inversa à primeira!", "Calculadora diz:");
+                return false;
+            }
+        }
+
+        MessageBoxResult result = MessageBox.Show("Essa matriz é inversa à segunda!", "Calculadora diz:");
+        return true;
+    
+    }
+
 }
+
+//I hate everything about you - Three Days Grace
